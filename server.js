6eb5ee9b97997bb96033526e00f093ba809ed3b8
McKB <mckennakb@comcast.net>
2022-05-21 17:06:09 -0400

    getResorts.pug partial started, index.pug started, server updated

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express()
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
+app.get('/', (request, response) => {
+  return response.render('index')
+})
+
 app.listen(1337, () => {
   console.log('listening at http://localhost:1337....') // eslint-disable-line no-console
 })
