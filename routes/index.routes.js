506395f574967dbd75ed83990397a0ce240502e7
McKB <mckennakb@comcast.net>
2022-06-05 21:13:53 -0400

    completes index route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,7 @@
 const express = require('express')
+const getDocumentation = require('../controllers/index.ctrl')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  return res.render('index')
-})
+router.get('/', getDocumentation)
 
 module.exports = router
