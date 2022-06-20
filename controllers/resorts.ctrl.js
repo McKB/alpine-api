adfeb691d722176544af627d2424da8f2fd05528
McKB <mckennakb@comcast.net>
2022-06-20 16:12:39 -0400

    resort GETS working

--- controllers/resorts.ctrl.js
+++ controllers/resorts.ctrl.js
@@ -1,10 +1,35 @@
 const models = require('../models/index.model')
 
-const getAllResorts = () => {
+const getAllResorts = async (req, res) => {
+  try {
+    const resorts = await models.Resorts.findAll()
 
+    return resorts
+      ? res.status(200).send(resorts)
+      : res.sendStatus(404)
+  }
+  catch (error) {
+    return res.sendStatus(500)
+  }
 }
-const getResortByNameWithStats = () => {
+const getResortByNameWithStats = async (req, res) => {
+  try {
+    const { name } = req.params
 
+    const resorts = await models.Resorts.findAll({
+      where: { resortName: { [models.Op.like]: `%${name}%` } },
+      include: [{ model: models.Stats }]
+    })
+
+    if (resorts.length !== 0) {
+      return res.status(200).send(resorts)
+    } else {
+      return res.status(404).send('Resort does not exist.')
+    }
+  }
+  catch (error) {
+    return res.sendStatus(500)
+  }
 }
 const addResort = () => {
 
