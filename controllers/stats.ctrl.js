e5ea70accbea5f6968eb95e10706c7ab9d124dd2
McKB <mckennakb@comcast.net>
2022-06-20 22:26:38 -0400

    both stats GET methods working

--- controllers/stats.ctrl.js
+++ controllers/stats.ctrl.js
@@ -1,11 +1,38 @@
 const models = require('../models/index.model')
 
-const getAllStatsWithResorts = () => {
+const getAllStatsWithResorts = async (req, res) => {
+  try {
+    const stats = await models.Stats.findAll({
+      include: [{ model: models.Resorts }]
+    })
 
+    return res.status(200).send(stats)
+  }
+  catch (error) {
+    return res.sendStatus(500)
+  }
 }
-const getStatsByResortIdWithResorts = () => {
 
+const getStatsByResortIdWithResorts = async (req, res) => {
+  try {
+    const { resortId } = req.params
+
+    const stats = await models.Stats.findOne({
+      where: { resortId },
+      include: [{ model: models.Resorts }]
+    })
+
+    if (!stats) {
+      return res.status(404).send('Stats do not exist for this resort or the resort does not exist.')
+    }
+
+    return res.status(200).send(stats)
+  }
+  catch (error) {
+    return res.sendStatus(500)
+  }
 }
+
 const addNewStats = () => {
 
 }
