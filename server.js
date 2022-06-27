2b1b0d6281eb45729f5c0847813cdf88c345548e
McKB <mckennakb@comcast.net>
2022-06-26 21:45:07 -0400

    updates api routes to include '/api' and updates documentation

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const chairliftsRouter = require('./routes/chairlifts.routes')
 const statsRouter = require('./routes/stats.routes')
 const indexRouter = require('./routes/index.routes')
 
-app.use('/resorts', resortsRouter)
-app.use('/chairlifts', chairliftsRouter)
-app.use('/stats', statsRouter)
-app.use('/', indexRouter)
+app.use('/api/resorts', resortsRouter)
+app.use('/api/chairlifts', chairliftsRouter)
+app.use('/api/stats', statsRouter)
+app.use('/api', indexRouter)
 
 app.listen(1337, () => {
   console.log('listening at http://localhost:1337....') // eslint-disable-line no-console
