ab43bfd974a69ae3ebbc685fdee45de97bb876fa
McKB <mckennakb@comcast.net>
2022-07-17 16:44:42 -0400

    adds error page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import HomePage from "./pages/HomePage/HomePage"
 import SearchPage from './pages/SearchPage/SearchPage'
 import ResortPage from './pages/ResortPage/ResortPage'
+import ErrorPage from "./pages/ErrorPage/ErrorPage"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Route path='/browse/:resort' element={<ResortPage />} />
           <Route path='/browse' element={<SearchPage />} />
           <Route path='/' element={<HomePage />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
     </div>
