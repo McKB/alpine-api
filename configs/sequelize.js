3b4404bce45ce84d0e6e8aa2d4f642f25d3c7e15
McKB <mckennakb@comcast.net>
2022-12-14 21:38:20 -0500

    pg changes

--- configs/sequelize.js
+++ configs/sequelize.js
@@ -4,13 +4,13 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'postgres',
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'postgres',
   }
 }
