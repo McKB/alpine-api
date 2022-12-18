430441ea56db3fa6a50f69f402fe19b4a71f8ff5
McKB <mckennakb@comcast.net>
2022-12-18 11:33:29 -0500

    updates About page

--- client/src/components/SourceCard.jsx
+++ client/src/components/SourceCard.jsx
@@ -2,16 +2,21 @@ import {Card, Col} from 'react-bootstrap'
 
 const SourceCard = (props) => {
 
-    const { link, text } = props
+    const { link, text, image } = props
 
     return (
-        <Col>
-            <Card>
+        <Col md={4} className='mb-5'>
+            <Card className='w-100'>
+                <Card.Img variant="top" src={image} style={{ width: '100%', overflow: 'hidden'}}/>
                 <Card.Body>
                     <Card.Text>
                         {text}
                     </Card.Text>
-                    <Card.Link className='fw-light' href={link}>Visit site</Card.Link>
+                    <Card.Link 
+                        className='fw-light' 
+                        href={link} 
+                        target="_blank"
+                        rel="noopener noreferrer">Visit site</Card.Link>
                 </Card.Body>
             </Card>
         </Col>
--- client/src/pages/AboutPage/AboutPage.jsx
+++ client/src/pages/AboutPage/AboutPage.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import BackButton from '../../components/BackButton'
 import Footer from '../../components/Footer'
 import SourceCard from '../../components/SourceCard'
+import source_1_img from '../../images/source_1_img.png'
+import source_2_img from '../../images/source_2_img.png'
+import source_3_img from '../../images/source_3_img.png'
+import source_4_img from '../../images/source_4_img.png'
+import source_5_img from '../../images/source_5_img.png'
+import source_6_img from '../../images/source_6_img.png'
 
 
 const AboutPage = () => {
@@ -27,14 +35,21 @@ const AboutPage = () => {
                 </Col>
             </Row>
             <Row className='m-4'>
-                <SourceCard link='https://geodata.vermont.gov/datasets/VCGI::vt-data-e911-alpine-ski-lifts-1/explore?location=44.463069%2C-72.766080%2C11.00' text='VT Open GeoData Portal'/>
-                <SourceCard link='https://www.arcgis.com/sharing/rest/content/items/af4c05d7941f43708f55e338330cdf0b/info/metadata/metadata.xml?format=default&output=html' text='EmergencyE911_ALPINELIFTS dataset' />
-                <SourceCard link='http://www.hackvt.org/datasets' text='HackVT list of available datasets' />
+                <SourceCard link='https://geodata.vermont.gov/datasets/VCGI::vt-data-e911-alpine-ski-lifts-1/explore?location=44.463069%2C-72.766080%2C11.00' text='VT Open GeoData Portal' image={source_1_img}/>
+                <SourceCard link='https://www.arcgis.com/sharing/rest/content/items/af4c05d7941f43708f55e338330cdf0b/info/metadata/metadata.xml?format=default&output=html' text='EmergencyE911_ALPINELIFTS dataset' image={source_2_img} />
+                <SourceCard link='http://www.hackvt.org/datasets' text='HackVT list of available datasets' image={source_3_img} />
             </Row>
             <Row className='m-4'>
-                <SourceCard link='https://www.vermontvacation.com/' text='Vermont Tourism' />
-                <SourceCard link='https://skivermont.com/' text='Vermont Ski Areas Association' />
-                <SourceCard link={'/browse'} text='Links to each resort homepage' />
+                <SourceCard link='https://accd.vermont.gov/tourism' text='Vermont Tourism' image={source_4_img} />
+                <SourceCard link='https://skivermont.com/' text='Vermont Ski Areas Association' image={source_5_img} />
+                <SourceCard link={'/browse'} text='Links to each resort homepage' image={source_6_img} />
+            </Row>
+            <Row className='m-4 justify-content-md-center'>
+                <Col md={4}>
+                    <Link to={'/'}>
+                        <BackButton previousLocation='home' color='light'/>
+                    </Link>
+                </Col>
             </Row>
             <Row className='pt-5 bottom-0'>
                 <Footer/>
